fa4c8c1edfe21f362a3566b1c14a04580acd9ae8
Convergente Studio <convergentepro@gmail.com>
2024-01-22 15:26:27 +0000

    Astro Settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,38 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	devToolbar: {
+		enabled: true
+	},
+	outDir: './dist',
+	publicDir: './public',
+	srcDir: './src',
+	prefetch: true,
+	integrations: [],
+	trailingSlash: 'always',
+
+	vite: {
+		clearScreen: true,
+		appType: 'custom',
+		logLevel: 'info',
+		css: {
+			devSourcemap: true,
+			modules: {
+				generateScopedName: '[name]__[local]___[hash:base64:5]'
+			}
+		},
+		optimizeDeps: {
+			exclude: ['@astrojs/astro'],
+			extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.scss', '.json']
+		}
+	},
+
+	build: {
+		format: 'directory'
+	},
+	server: {
+		port: 4321,
+		open: true
+	}
+});
